472650d5dd34400925ddb65c52e6b6710e5fa212
kalid <abdur033@umn.edu>
2025-07-02 11:51:24 -0500

    blocked august days

--- script.js
+++ script.js
@@ -595,13 +595,12 @@ pickupDateField.addEventListener("change", function(e) {
   // 2) Block these exact dates: June 11, 12, 14, 2025
   //    (format: "YYYY-MM-DD")
   const invalidDates = [
-    "2025-06-09",
-    "2025-06-10",
-    "2025-06-11",
-    "2025-06-12",
-    "2025-06-13",
-    "2025-06-14",
-    "2025-06-15"
+    "2025-08-05",
+    "2025-08-06",
+    "2025-08-07",
+    "2025-08-08",
+    "2025-08-09",
+    "2025-08-10",
   ];
   if (invalidDates.includes(val)) {
     showFormError("⚠️ That date is unavailable. Please choose another day.");
